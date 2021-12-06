7cd24da2540027b02514056af68e4a53c610ef04
skarner2016 <skarner2016@gmail.com>
2021-12-06 17:47:49 +0800

    project init

---
+++ app/Console/Commands/Test.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:Test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command Test';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $res = DB::table("apis")->first();
+
+        dd($res ?? '$res === null');
+        return Command::SUCCESS;
+    }
+}
